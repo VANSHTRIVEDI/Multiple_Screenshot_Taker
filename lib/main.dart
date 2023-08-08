6dcb9375f735b40b50d5fb3e473b5b3e342a06f6
VANSH TRIVEDI <vanshtrivedi12345@gmail.com>
2023-08-09 01:27:46 +0530

    ScreenShot Taker

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  final GlobalKey _key = GlobalKey();
+
+  void _CaptureScreenShot() async {
+
+    for(int i =0;i<20;i++)
+    {
+
+    
+    //get paint bound of your app screen or the widget which is wrapped with RepaintBoundary.
+    RenderRepaintBoundary bound =
+        _key.currentContext!.findRenderObject() as RenderRepaintBoundary;
+
+    if (bound.debugNeedsPaint) {
+      Timer(Duration(seconds: 1), () => _CaptureScreenShot());
+      return null;
+    }
+
+    ui.Image image = await bound.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    // this will save image screenshot in gallery
+    if (byteData != null) {
+      Uint8List pngBytes = byteData.buffer.asUint8List();
+      final resultsave = await ImageGallerySaver.saveImage(
+          Uint8List.fromList(pngBytes),
+          quality: 90,
+          name: 'screenshot-${DateTime.now()}.png');
+      print(resultsave);
+    
+    }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Here i have wrapped whole app screen scaffold widget, to take full screenshot
+    return RepaintBoundary(
+      key: _key,
+      child: Scaffold(
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+             
+              child:Center(child:Text('THIS IS A CONTAINER')),
+              color: Colors.white,
+              width: 220,
+              height: 220,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _CaptureScreenShot(); // Method called to take screenshot on wrapped widget and save it.
+                },
+                child: Text("Capture & Save"))
+          ],
+        )),
+      ),
+    );
+  }
+}
